2f8ed198600abc2812fd4b6fb2ca3aa800bb6b22
Jake Craige <james.craige@gmail.com>
2016-03-25 09:00:43 -0700

    Initial Commit

---
+++ FollowUp/FollowUp.h
@@ -0,0 +1,19 @@
+//
+//  FollowUp.h
+//  FollowUp
+//
+//  Created by James Craige on 3/25/16.
+//  Copyright © 2016 thoughtbot. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for FollowUp.
+FOUNDATION_EXPORT double FollowUpVersionNumber;
+
+//! Project version string for FollowUp.
+FOUNDATION_EXPORT const unsigned char FollowUpVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FollowUp/PublicHeader.h>
+
+
---
+++ README.md
@@ -0,0 +1,4 @@
+# FollowUp
+
+This AirMail Plugin helps you to remember to follow up with people using the snooze
+feature.
